f7a055b5e475a126557e3cf04b04f688ab573d53
guoqing2013 <guoqing2013@163.com>
2018-05-19 17:14:55 +0800

    koa-react-project

--- server/src/controllers/goods/unit.controller.js
+++ server/src/controllers/goods/unit.controller.js
@@ -1,79 +1,112 @@
 // import jwt from 'jsonwebtoken'
+import joi from 'joi'
 import Unit from '../../models/goods/unit'
 
-export const create = async (ctx) => {
-  const data = ctx.request.body
-  console.log(data)
-  try {
-    const sku = await Unit.create({
+const unitSchemaCreate = joi.object({
+  unit_name: joi
+    .string()
+    .min(1)
+    .max(20)
+    .required(),
+  source: joi
+    .string()
+    .required(),
+  status: joi
+    .number()
+    .integer(),
+  userId: joi
+    .number()
+    .integer()
+    .required(),
+  ipAddress: joi
+    .string()
+    .required()
+})
+
+class UnitController {
+  static async create (ctx) {
+    const body = ctx.request.body
+
+    // Attach logged in user
+    //  const user = new User(ctx.state.user)
+    //  body.userId = user.id
+    body.userId = 1
+
+    // Add ip
+    body.ipAddress = ctx.ip
+    console.log(body)
+
+    // Validate the newly created note
+    const validator = joi.validate(body, unitSchemaCreate)
+    if (validator.error) {
+      ctx.throw(400, validator.error.details[0].message)
+    }
+
+    try {
+      const sku = await Unit.create({
+        source: body.source,
+        status: body.status,
+        unit_name: body.unit_name
+      })
+      ctx.body = { unit_id: sku.get('unit_id') }
+    } catch (error) {
+      console.log(error)
+      ctx.throw(400, 'INVALID_DATA')
+    }
+  }
+
+  static async update (ctx) {
+    const data = ctx.request.body
+    console.log('updateData', data)
+    return Unit.update({
       source: data.source,
       status: data.status,
       unit_name: data.unit_name
-    })
-    ctx.body = {
-      unit_id: sku.get('unit_id')
-    }
-  } catch (e) {
+    }, {where: {unit_id: data.unit_id}})
+      .then((result) => {
+        // here your result is simply an array with number of affected rows
+        console.log(result)
+        ctx.body = {
+          response: !!result[0]
+        }
+      })
+  }
 
+  static async delete (ctx) {
+    const data = ctx.request.body
+    console.log('delete data', data)
+    return Unit.destroy({where: {unit_id: data.unit_ids, source: data.source}})
+      .then((result) => {
+        console.log('deleted skus length:', result)
+        ctx.body = {
+          response: result
+        }
+      })
+  }
+
+  static async search (ctx) {
+    let pageNo = ctx.query.page_no
+    let pageSize = 20 || parseInt(ctx.query.page_size)
+    let offset = pageSize * (pageNo - 1)
+    console.log(pageNo, pageSize, offset)
+    return Unit.findAndCountAll({
+      limit: pageSize,
+      offset: offset
+    }).then((result) => {
+      ctx.body = {
+        'response': {
+          'paginator': {
+            pageSize: pageSize,
+            page: pageNo,
+            'totalCount': result.count
+          },
+          'items': result.rows
+        }
+      }
+    }).catch((error) => {
+      console.log(error)
+    })
   }
 }
 
-export const update = async (ctx) => {
-  const data = ctx.request.body
-  console.log('updateData', data)
-  return Unit.update({
-    source: data.source,
-    status: data.status,
-    unit_name: data.unit_name
-  }, {where: {unit_id: data.unit_id}})
-    .then((result) => {
-      // here your result is simply an array with number of affected rows
-      console.log(result)
-      ctx.body = {
-        response: !!result[0]
-      }
-    })
-}
-
-const deletes = (ctx) => {
-  const data = ctx.request.body
-  console.log('delete data', data)
-  return Unit.destroy({where: {unit_id: data.unit_ids, source: data.source}})
-    .then((result) => {
-      console.log('deleted skus length:', result)
-      ctx.body = {
-        response: result
-      }
-    })
-}
-
-export const search = (ctx) => {
-  let pageNo = ctx.query.page_no
-  let pageSize = 20 || parseInt(ctx.query.page_size)
-  let offset = pageSize * (pageNo - 1)
-  console.log(pageNo, pageSize, offset)
-  return Unit.findAndCountAll({
-    limit: pageSize,
-    offset: offset
-  }).then((result) => {
-    ctx.body = {
-      'response': {
-        'paginator': {
-          pageSize: pageSize,
-          page: pageNo,
-          'totalCount': result.count
-        },
-        'items': result.rows
-      }
-    }
-  }).catch((error) => {
-    console.log(error)
-  })
-}
-
-export default {
-  create,
-  update,
-  deletes,
-  search
-}
+export default UnitController
--- server/src/routes/goods/unit.js
+++ server/src/routes/goods/unit.js
@@ -8,7 +8,7 @@ router.post('/api/oauthentry/youzan.retail.product.unit/3.0.0/create', unit.crea
 // 编辑单位字典
 router.post('/api/oauthentry/youzan.retail.product.unit/3.0.0/update', unit.update)
 // 批量删除商品单位
-router.post('/api/oauthentry/youzan.retail.product.units/3.0.0/delete', unit.deletes)
+router.post('/api/oauthentry/youzan.retail.product.units/3.0.0/delete', unit.delete)
 // 分页查询商品单位
 router.get('/api/oauthentry/youzan.retail.product.units/3.0.0/search', unit.search)
 
