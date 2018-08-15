f506bd3220bbbf44438e0a9bb7e31c07b820e048
guoqing2013 <guoqing2013@163.com>
2018-08-15 23:15:08 +0800

    uploadImage

---
+++ vue-element-admin/src/components/UploadMultiImage/index.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="uploadMultiImage-container">
+    <el-button icon='el-icon-upload' size="mini" :style="{background:color,borderColor:color}" @click=" dialogVisible=true" type="primary">上传图片
+    </el-button>
+    <el-dialog :visible.sync="dialogVisible">
+    <!--
+      <el-upload 
+      class="editor-slide-upload"
+  action="https://jsonplaceholder.typicode.com/posts/"
+        :multiple="true"
+         :file-list="fileList"
+          :show-file-list="true"
+        list-type="picture-card" :on-remove="handleRemove" :on-success="handleSuccess" :before-upload="beforeUpload">
+        <el-button size="small" type="primary">点击上传</el-button>
+      </el-upload>
+
+
+        action="https://jsonplaceholder.typicode.com/posts/"
+       action="https://httpbin.org/post"
+      -->
+
+      <el-upload
+  class="upload-demo"
+  ref="upload"
+       action="https://jsonplaceholder.typicode.com/posts/"
+  :on-preview="handlePreview"
+  :on-remove="handleRemove"
+  :file-list="fileList"
+  :show-file-list="true"
+   list-type="picture-card"
+  :auto-upload="true">
+  <el-button slot="trigger" size="small" type="primary">选取文件</el-button>
+  <el-button style="margin-left: 10px;" size="small" type="success" @click="submitUpload">上传到服务器</el-button>
+  <div slot="tip" class="el-upload__tip">只能上传jpg/png文件，且不超过500kb</div>
+
+
+</el-upload>
+      <el-button @click="dialogVisible = false">取 消</el-button>
+      <el-button type="primary" @click="handleSubmit">确 定</el-button>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+// import { getToken } from 'api/qiniu'
+
+export default {
+  name: 'editorSlideUpload',
+  props: {
+    color: {
+      type: String,
+      default: '#1890ff'
+    }
+  },
+  data() {
+    return {
+      dialogVisible: false,
+      listObj: {},
+      fileList: []
+    }
+  },
+  methods: {
+    checkAllSuccess() {
+      return Object.keys(this.listObj).every(item => this.listObj[item].hasSuccess)
+    },
+     submitUpload() {
+        this.$refs.upload.submit();
+      },
+    handleSubmit() {
+      const arr = Object.keys(this.listObj).map(v => this.listObj[v])
+      if (!this.checkAllSuccess()) {
+        this.$message('请等待所有图片上传成功 或 出现了网络问题，请刷新页面重新上传！')
+        return
+      }
+      console.log(arr)
+      this.$emit('successCBK', arr)
+      this.listObj = {}
+      this.fileList = []
+      this.dialogVisible = false
+    },
+    handleSuccess(response, file) {
+      const uid = file.uid
+      const objKeyArr = Object.keys(this.listObj)
+      for (let i = 0, len = objKeyArr.length; i < len; i++) {
+        if (this.listObj[objKeyArr[i]].uid === uid) {
+          this.listObj[objKeyArr[i]].url = response.files.file
+          this.listObj[objKeyArr[i]].hasSuccess = true
+          return
+        }
+      }
+    },
+    handleRemove(file) {
+      const uid = file.uid
+      const objKeyArr = Object.keys(this.listObj)
+      for (let i = 0, len = objKeyArr.length; i < len; i++) {
+        if (this.listObj[objKeyArr[i]].uid === uid) {
+          delete this.listObj[objKeyArr[i]]
+          return
+        }
+      }
+    },
+    beforeUpload(file) {
+      const _self = this
+      const _URL = window.URL || window.webkitURL
+      const fileName = file.uid
+      this.listObj[fileName] = {}
+      return new Promise((resolve, reject) => {
+        const img = new Image()
+        img.src = _URL.createObjectURL(file)
+        img.onload = function() {
+          _self.listObj[fileName] = { hasSuccess: false, uid: file.uid, width: this.width, height: this.height }
+        }
+        resolve(true)
+      })
+    }
+  }
+}
+</script>
+
+<style rel="stylesheet/scss" lang="scss" scoped>
+.uploadMultiImage-container {
+  .el-upload-list--picture-card .el-upload-list__item {
+    width: 80px;
+    height: 80px;
+  }
+}
+.editor-slide-upload {
+  margin-bottom: 20px;
+  /deep/ .el-upload--picture-card {
+    width: 100%;
+  }
+}
+</style>
--- vue-element-admin/src/views/goods/components/ArticleDetail.vue
+++ vue-element-admin/src/views/goods/components/ArticleDetail.vue
@@ -12,34 +12,53 @@
       </sticky>
 
       <div class="createPost-main-container">
+        <!-- 基本信息 -->
+        <div>
+          <h3 class="guide">基本信息</h3>
+
+          <el-row>
+            <el-col :span="10">
+              <el-form-item prop="title" label-width="80px" label="商品名：">
+                <el-input name="title" v-model="postForm.title" required :maxlength="100"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
 
 
-        <el-row >
-          <el-col :span="10">
-            <el-form-item prop="title" label-width="80px" label="商品名：">
-              <el-input name="title" v-model="postForm.title" required :maxlength="100"></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
 
-        <el-row>
-          <el-col :span="10">
-            <el-form-item prop="price" label-width="80px" label="价格：">
-              <el-input name="price" v-model="postForm.price" required></el-input>
-            </el-form-item>
-          </el-col>
-        </el-row>
+          <el-row>
+            <el-col :span="10">
+              <el-form-item prop="price" label-width="80px" label="商品图：">
+                  <el-button>添加图片</el-button>
+              </el-form-item>
+              <div class="help-block">建议尺寸：800*800像素，你可以拖拽图片调整顺序，最多上传15张</div>
+              <UploadMultiImage />
+            </el-col>
+          </el-row>
 
-        <el-row>
-          <el-col :span="10">
-            <el-form-item prop="price" label-width="80px" label="商品图：">
-              <el-input name="price" v-model="postForm.price" required></el-input>
-            </el-form-item>
-            <div class="sub-title">建议尺寸：800*800像素，你可以拖拽图片调整顺序，最多上传15张</div>
-          </el-col>
-        </el-row>
+          <el-row>
+            <el-col :span="10">
+              <el-form-item prop="sell_point" label-width="80px" label="商品卖点：">
+                <el-input name="sell_point" v-model="postForm.sell_point"></el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
 
+        </div>
 
+        <!-- 价格库存 -->
+        <div>
+          <h3 class="createPost-guide">价格库存</h3>
+          <el-row>
+            <el-col :span="10">
+              <el-form-item prop="price" label-width="80px" label="价格：">
+                <el-input v-model="postForm.price">
+                  <template slot="prepend">¥</template>
+                </el-input>
+              </el-form-item>
+            </el-col>
+          </el-row>
+        </div>
 
 
         <el-row>
@@ -109,6 +128,7 @@
   import Multiselect from 'vue-multiselect' // 使用的一个多选框组件，element-ui的select不能满足所有需求
   import 'vue-multiselect/dist/vue-multiselect.min.css' // 多选框组件css
   import Sticky from '@/components/Sticky' // 粘性header组件
+  import UploadMultiImage from '@/components/UploadMultiImage' 
   import {
     validateURL
   } from '@/utils/validate'
@@ -130,8 +150,9 @@
 
   const defaultForm = {
     status: 'draft',
-    title: '', // 文章题目
+    title: '',
     price: '', //价格
+    sell_point: '', //商品卖点
     content: '', // 文章内容
     content_short: '', // 文章摘要
     source_uri: '', // 文章外链
@@ -154,7 +175,8 @@
       Warning,
       CommentDropdown,
       PlatformDropdown,
-      SourceUrlDropdown
+      SourceUrlDropdown,
+      UploadMultiImage
     },
     props: {
       isEdit: {
@@ -200,7 +222,8 @@
             message: '商品名称必须填写，最多100个字'
           }],
           price: [{
-            validator: validateRequire
+            required: true,
+            message: '请输入价格'
           }],
           // content: [{ validator: validateRequire }],
           // source_uri: [{ validator: validateSourceUri, trigger: 'blur' }]
@@ -286,10 +309,42 @@
 
 </script>
 
+<style>
+  .el-form-item__label {
+    font-size: 12px !important;
+    font-weight: normal !important;
+  }
+
+</style>
+
 <style rel="stylesheet/scss" lang="scss" scoped>
   @import "src/styles/mixin.scss";
+
+
+
   .createPost-container {
     position: relative;
+
+
+    .guide {
+      background-color: #f8f8f8;
+      font-size: 14px;
+      font-weight: 700;
+      padding: 0 0 0 10px;
+      height: 38px;
+      line-height: 38px;
+    }
+
+    .help-block {
+      line-height: 14px;
+      font-size: 12px;
+      margin-top: 6px;
+      margin-bottom: 0;
+      color: #999;
+      margin-left: 80px;
+    }
+
+
     .createPost-main-container {
       padding: 40px 45px 20px 50px;
       .postInfo-container {
