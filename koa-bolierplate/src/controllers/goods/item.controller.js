7b1409834cd13697376a7636b3cf1a001d2a2c64
guoqing2013 <guoqing2013@163.com>
2018-08-19 16:59:20 +0800

    project

--- koa-bolierplate/src/controllers/goods/item.controller.js
+++ koa-bolierplate/src/controllers/goods/item.controller.js
@@ -77,14 +77,14 @@ export const updateSku = async (ctx) => {
 }
 
 /**
- * 获取商品库列表
+ * 获取出售中的商品列表
  */
-export const getSkus = (ctx) => {
-  let pageNo = ctx.query.page_no
-  let pageSize = 20 || parseInt(ctx.query.page_size)
+export const getItems = (ctx) => {
+  const data = ctx.query;
+  let pageNo = data.page_no
+  let pageSize = 20 || parseInt(data.page_size)
   let offset = pageSize * (pageNo - 1)
-  console.log(pageNo, pageSize, offset)
-  return Sku.findAndCountAll({
+  return Item.findAndCountAll({
     limit: pageSize,
     offset: offset
   }).then((result) => {
@@ -93,33 +93,17 @@ export const getSkus = (ctx) => {
     // offset = limit * (page - 1)
 
     ctx.body = {
-      'response': {
-        'paginator': {
-          pageSize: pageSize,
-          page: pageNo,
-          'totalCount': result.count
-        },
-        'items': result.rows
+      response: {
+        count: result.count,
+        items: result.rows
       }
     }
   }).catch((error) => {
     console.log(error)
     // res.status(500).send('Internal Server Error')
   })
-  // const pageSize = param.page_size || 10
-  // const page = param.page_no || 1
-  // const totalCount = Math.ceil(result.count / limit)
-  // offset = limit * (page - 1)
-  // ctx.body.status = 200
-  // .catch(function (error) {
-  //   ctx.body.status = 500
-  // })
-
-  // ctx.body = {'response': {'paginator': {'pageSize': 20, 'page': 1, 'totalCount': 2}, 'items': [{'kdtId': 40497547, 'sellChannel': 1, 'lastCostPrice': 0, 'avgCostPrice': 0, 'overSoldNum': 0, 'specifications': '33', 'photoUrl': '[{"url":"https://img.yzcdn.cn/public_files/2017/08/30/63a8d28bce4ca2e5d081e1e69926288e.jpg"}]', 'createdAt': 1520915504000, 'unit': '件', 'stockLowWarning': true, 'sellStockCount': 0, 'name': '444444444', 'stockNum': 0, 'skuNo': '1234', 'categoryId': 305936, 'skuId': 5907196, 'status': 0, 'updatedAt': 1520915504000, 'stockHighWarning': false}, {'kdtId': 40497547, 'sellChannel': 6, 'lastCostPrice': 0, 'avgCostPrice': 0, 'overSoldNum': 0, 'specifications': '', 'photoUrl': '[{"url":"https://img.yzcdn.cn/upload_files/2015/05/14/FoMCEwRpIbleJqCh7A---MZ-JvUc.png"}]', 'createdAt': 1520734552000, 'unit': '件', 'stockLowWarning': false, 'sellStockCount': 100000, 'name': '实物商品（购买时需填写收货地址，测试商品，不发货，不退款）', 'stockNum': 100000, 'skuNo': 'P000000000000001', 'categoryId': 305936, 'skuId': 5903147, 'status': 0, 'updatedAt': 1520764795000, 'stockHighWarning': false}]}}
-
-  // ctx.body = {'response': {'paginator': {'pageSize': 10, 'page': 1, 'totalCount': 25}, 'items': result}}
-  // console.log(goodslist)
 }
+
 
 const deleteSkus = (ctx) => {
   const data = ctx.request.body
@@ -135,8 +119,7 @@ const deleteSkus = (ctx) => {
 
 export default {
   createItem,
-
-  getSkus,
+  getItems,
   getSku,
   updateSku,
   deleteSkus
--- koa-bolierplate/src/routes/goods/item.js
+++ koa-bolierplate/src/routes/goods/item.js
@@ -5,5 +5,7 @@ const router = Router()
 
 // 新增商品
 router.post('/api/oauthentry/youzan.item/3.0.0/create', item.createItem)
+// 获取出售中的商品列表
+router.get('/api/oauthentry/youzan.items.onsale/3.0.0/get', item.getItems)
 
 export default router
